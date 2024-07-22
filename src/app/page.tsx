b86fe8850639b538d6fa5e86b3ef79b49c36595e
Jesus Santiago Carrasco Campa <jsanticc@hotmail.com>
2024-07-22 13:51:34 -0700

    feat: add 3 new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Header from '@/sections/Header';
 import Hero from '@/sections/Hero';
+import LogoTicker from '@/sections/LogoTicker';
+import Pricing from '@/sections/Pricing';
+import ProductShowcase from '@/sections/ProductShowcase';
 
 const Home = () => (
   <>
     <Header />
     <Hero />
+    <LogoTicker />
+    <ProductShowcase />
+    <Pricing />
   </>
 );
 
