84475463ede51f0a42d57c39f188d9e848f0d7aa
Jesus Santiago Carrasco Campa <jsanticc@hotmail.com>
2024-07-22 13:52:28 -0700

    feat: create new sections

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,55 +1,74 @@
-const pricingTiers = [
-  {
-    title: "Free",
-    monthlyPrice: 0,
-    buttonText: "Get started for free",
-    popular: false,
-    inverse: false,
-    features: [
-      "Up to 5 project members",
-      "Unlimited tasks and projects",
-      "2GB storage",
-      "Integrations",
-      "Basic support",
-    ],
-  },
-  {
-    title: "Pro",
-    monthlyPrice: 9,
-    buttonText: "Sign up now",
-    popular: true,
-    inverse: true,
-    features: [
-      "Up to 50 project members",
-      "Unlimited tasks and projects",
-      "50GB storage",
-      "Integrations",
-      "Priority support",
-      "Advanced support",
-      "Export support",
-    ],
-  },
-  {
-    title: "Business",
-    monthlyPrice: 19,
-    buttonText: "Sign up now",
-    popular: false,
-    inverse: false,
-    features: [
-      "Up to 5 project members",
-      "Unlimited tasks and projects",
-      "200GB storage",
-      "Integrations",
-      "Dedicated account manager",
-      "Custom fields",
-      "Advanced analytics",
-      "Export capabilities",
-      "API access",
-      "Advanced security features",
-    ],
-  },
-];
+import { twMerge } from 'tailwind-merge';
 
-export const Pricing = () => {
-  return null;
-};
+import CheckIcon from '@/assets/check.svg';
+import { pricingTiers } from '@/constants/pricing';
+
+const Pricing = () => (
+  <section className="py-24">
+    <div className="container">
+      <div className="section-container">
+        <h2 className="section-title text-center">Pricing</h2>
+        <p className="section-description mt-5">
+          Free forever. Upgrade for unlimited tasks, better security, and
+          exclusive features
+        </p>
+      </div>
+      <div className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center">
+        {/* Card */}
+        {pricingTiers.map((tier, i) => (
+          <div
+            key={i}
+            className={twMerge(
+              'p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA] max-w-xs w-full',
+              tier.inverse && 'border-black bg-black text-white'
+            )}
+          >
+            <div className="flex justify-between">
+              <h3
+                className={twMerge(
+                  'text-lg font-bold text-black/50',
+                  tier.inverse && 'text-white/60'
+                )}
+              >
+                {tier.title}
+              </h3>
+              {tier.popular && (
+                <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
+                  <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium">
+                    Popular
+                  </span>
+                </div>
+              )}
+            </div>
+            <div className="flex items-baseline gap-1 mt-[30px]">
+              <span className="text-4xl font-bold tracking-tighter leading-none">
+                ${tier.monthlyPrice}
+              </span>
+              <span className="tracking-tight font-bold text-black/50">
+                /month
+              </span>
+            </div>
+            <button
+              className={twMerge(
+                'btn btn-primary w-full mt-[30px]',
+                tier.inverse && 'bg-white text-black'
+              )}
+            >
+              {tier.buttonText}
+            </button>
+            <ul className="flex flex-col gap-5 mt-8">
+              {tier.features.map((feature, j) => (
+                <li key={j} className="text-sm flex items-center gap-4">
+                  <CheckIcon className="h-6 w-6" />
+                  <span>{feature}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  </section>
+);
+
+export default Pricing;
