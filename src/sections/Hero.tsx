6e0055a03912bdcbb68433f653c91c0f00e9440c
Jesus Santiago Carrasco Campa <jsanticc@hotmail.com>
2024-07-22 13:52:15 -0700

    refactor(style): apply custom utility class

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,10 +10,8 @@ const Hero = () => (
     <div className="container">
       <div className="md:flex items-center">
         <div className="md:w-[478px]">
-          <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
-            Version 2.0 is here
-          </div>
-          <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+          <div className="tag">Version 2.0 is here</div>
+          <h1 className="section-title text-5xl md:text-7xl mt-6">
             Pathway to productivity
           </h1>
           <p className="text-xl text-[#010D3E] tracking-tight mt-6">
