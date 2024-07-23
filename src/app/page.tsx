df0dee05c6cf58154b6aa13cbcb32bcbcb2cdfe6
Jesus Santiago Carrasco Campa <jsanticc@hotmail.com>
2024-07-23 11:49:52 -0700

    feat: add remaining sections of LP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import CallToAction from '@/sections/CallToAction';
+import Footer from '@/sections/Footer';
 import Header from '@/sections/Header';
 import Hero from '@/sections/Hero';
 import LogoTicker from '@/sections/LogoTicker';
 import Pricing from '@/sections/Pricing';
 import ProductShowcase from '@/sections/ProductShowcase';
+import Testimonials from '@/sections/Testimonials';
 
 const Home = () => (
   <>
@@ -11,6 +14,9 @@ const Home = () => (
     <LogoTicker />
     <ProductShowcase />
     <Pricing />
+    <Testimonials />
+    <CallToAction />
+    <Footer />
   </>
 );
 
